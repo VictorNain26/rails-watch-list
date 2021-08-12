827496ca74691a5bf13a5297c7659a445dfe2ba3
victor lenain <victor.lenain26@gmail.com>
2021-08-12 21:34:29 +0200

    finish rails

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,23 @@
+class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(params.require(:bookmark).permit(:comment, :movie_id))
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to @list
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to lists_path
+  end
+end
